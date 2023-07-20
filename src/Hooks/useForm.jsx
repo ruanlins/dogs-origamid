b1ca6b0575e0ca1f48d13ab252ddf7f25635b21c
Ruan Lins <ruanblins@gmail.com>
2023-07-20 20:11:30 -0300

    Added create user function and logout button

--- src/Hooks/useForm.jsx
+++ src/Hooks/useForm.jsx
@@ -2,8 +2,14 @@ import React from 'react';
 
 const types = {
   email: {
-    regex: /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+    regex:
+      /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
     message: 'Preencha um email válido',
+  },
+  password: {
+    regex: /^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,}$/,
+    message:
+      'A senha precisa ter no mínimo 8 caracteres, 1 caracter maíusculo, 1 minúsculo e 1 digito e 1 especial',
   },
 };
 
