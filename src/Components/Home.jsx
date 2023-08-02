8b20108dc1a5720c59faa08465c3ac4ffa269d6c
Ruan Lins <ruanblins@gmail.com>
2023-08-02 20:40:36 -0300

    Created Head component

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import Feed from './Feed/Feed';
-import styles from './Home.module.css';
-import Loading from './Helper/Loading';
+import Head from './Helper/Head';
 
 const Home = () => {
   return (
     <section className="container mainContainer">
+      <Head
+        title="Fotos"
+        description="Home do site Dogs, com o feed de fotos"
+      />
       <Feed />
     </section>
   );
--- src/Components/Login/LoginPasswordLost.jsx
+++ src/Components/Login/LoginPasswordLost.jsx
@@ -5,6 +5,7 @@ import useForm from '../../Hooks/useForm';
 import useFetch from '../../Hooks/useFetch';
 import Error from '../Helper/Error';
 import { PASSWORD_LOST } from '../../api';
+import Head from '../Helper/Head';
 
 const LoginPasswordLost = () => {
   const login = useForm();
@@ -23,6 +24,7 @@ const LoginPasswordLost = () => {
 
   return (
     <section>
+      <Head title="Perdeu a Senha" />
       <h1 className="title">Perdeu a senha?</h1>
       {data ? (
         <p>{data}</p>
